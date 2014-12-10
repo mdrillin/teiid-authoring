d81ef37933e3e983574d081b4bb7bd7856c7fd6a
Jonathan Fuerth <jfuerth@gmail.com>
2014-12-10 12:44:48 -0500

    Removed conflicting CSS and JavaScript libraries.
    UberFire requires Bootstrap 2 and jQuery 1.7, and provides both of these to the page.
    Apps must not include additional copies of Bootstrap or jQuery.

--- teiid-authoring-webapp/src/main/java/org/teiid/authoring/client/resources/AppCss.java
+++ teiid-authoring-webapp/src/main/java/org/teiid/authoring/client/resources/AppCss.java
@@ -18,14 +18,13 @@ package org.teiid.authoring.client.resources;
 import com.google.gwt.resources.client.ClientBundle;
 import com.google.gwt.resources.client.CssResource;
 import com.google.gwt.resources.client.CssResource.NotStrict;
-import com.google.gwt.resources.client.TextResource;
 
 public interface AppCss extends ClientBundle {
 
 	@NotStrict
     @Source("css/CustomToggle.css")
     CssResource customToggleStyle();
-	
+
 	@NotStrict
     @Source("css/DataGrid.css")
     CssResource customDataGridStyle();
@@ -33,7 +32,5 @@ public interface AppCss extends ClientBundle {
 	@NotStrict
     @Source("css/teiid-authoring.css")
     CssResource teiidAuthoringStyle();
-	
-	@Source("css/rcue.css") TextResource rcueCss();
 
 }
